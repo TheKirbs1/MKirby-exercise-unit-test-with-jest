c3ac688777ef9b90aa819ecbb15aac4e74885ae1
Matt Kirby <160439337+TheKirbs1@users.noreply.github.com>
2024-04-22 21:07:53 +0000

    EuroToDollar, DollarToYen, and YenToPound complete

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-// const sum = (a,b) => {
-//     return a+b;
-// }
-
-// module.exports = {sum};
-
-// We declare the function with the exact name "fromEuroToDollar"
-const fromEuroToDollar = function(valueInEuro) {
-    // Convert the given valueInEuro to dollars
-    let valueInDollar = valueInEuro * 1.07;
-    // return the dollar value
-    return valueInDollar;
-}
-
-let oneEuroIs = {
+ const oneEuroIs = {
     "JPY": 156.5, // japan yen
     "USD": 1.07, // us dollar
     "GBP": 0.87, // british pound
 }
 
-// We include fromEuroToDollar here as well because it needs to be exported
-module.exports = {fromEuroToDollar }
+const fromEuroToDollar = (euro) => {
+    return euro * oneEuroIs.USD
+}
+
+const fromDollarToYen = (Dollar) => {
+    return (Dollar * oneEuroIs.JPY) / oneEuroIs.USD
+}
+
+const fromYenToPound = (Yen) => {
+    return (Yen * oneEuroIs.GBP) / oneEuroIs.JPY
+}
+
+module.exports = {
+    fromDollarToYen,
+    fromEuroToDollar,
+    fromYenToPound
+ }
--- app.test.js
+++ app.test.js
@@ -1,18 +1,29 @@
-// const {sum} =require('./app');
-
-// test('add 7+3 to equal 10', ()=> {
-//     expect(sum(7,3)).toBe(10);
-// })
 test("One euro should be 1.07 dollars", function() {
-    // Import the function from app.js
     const { fromEuroToDollar } = require('./app.js');
 
-    // Use the function like its supposed to be used
     const dollars = fromEuroToDollar(3.5);
 
-    // If 1 euro is 1.07 dollars, then 3.5 euros should be (3.5 * 1.07)
-    const expected = 3.5 * 1.07; 
-    
-    // This is the comparison for the unit test
-     expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro is 1.07 dollars, then 3.5 euros should be = (3.5 * 1.07)
+    const expected = 3.5 * 1.07;
+
+    expect(fromEuroToDollar(3.5)).toBe(3.745); 
+})
+
+test("One dollar should be approximately 146.26 yens", function() {
+    const { fromDollarToYen } = require('./app.js');
+
+    const yens = fromDollarToYen(8);
+
+    const expectedYens = 8 * 146.261682;
+
+    expect(yens).toBeCloseTo(expectedYens,2); 
+})
+
+test("1 yen should be approximately 0.005559 pounds", function() {
+    const { fromYenToPound } = require('./app.js');
+
+    const pounds = fromYenToPound(8000);
+
+    const expectedPounds = 8000 * 0.005559;
+
+    expect(pounds).toBeCloseTo(expectedPounds, 2);
 })
